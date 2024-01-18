7f6833ec06b55fa702d2d072295cc7e668e89f19
Amit Singh <amit195@hotmail.com>
2024-01-18 19:30:48 +0530

    rhf zod demo code for form validation

---
+++ README.md
@@ -0,0 +1,2 @@
+How to Validate Forms with Zod and React-Hook-Form by Gift Uhiene for freeCodeCamp
+https://www.freecodecamp.org/news/react-form-validation-zod-react-hook-form/
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-"use client";
+'use client';
+import Form from '@/app/components/Form';
 
 function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello!</h1>
+      <Form />
     </main>
   );
 }
